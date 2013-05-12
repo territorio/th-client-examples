a411d9b051bda82eee8f558893645fec8973f95d
Pepe Cano <ppcanodehuelva@gmail.com>
2013-05-12 22:53:28 +0200

    Continue new Landing screen

--- app/app/lib/examples/landing/main.js
+++ app/app/lib/examples/landing/main.js
@@ -1,15 +1,46 @@
-console.log('main landing');
 
 Th.Application = Em.Application.extend({
 
   ready: function() {
 
+    this.initStore();
+    this.loadData();
+
     console.log('app is ready');
-    var view = Th.LandingScreenView.create();
+    var categories = App.store.findAll(Th.Category);
+    var events = App.store.findAll(Th.Event);
+
+    var view = Th.LandingScreenView.create({
+      container: this.__container__,
+      categories: categories,
+      //selectedCategory: App.store.find(Th.Category, 1),
+      selectedCategory: Th.CategoryAllType,
+      isAsideLeft: false,
+      events: events
+      //events: null
+    });
     view.appendTo('#init-app');
+
+    App.view = view;
+
+  },
+
+
+  loadData: function() {
+    App.store.findAll(Th.Category);
+    App.store.findAll(Th.Event);
+  },
+
+  initStore: function() {
+
+		this.store = DS.Store.create({
+			revision: 12,
+      adapter: 'DS.FixtureAdapter'
+		});
 
   }
 
 });
-
 App = Th.Application.create({});
+
+Ember.lookup.App = App;
--- app/app/lib/main.js
+++ app/app/lib/main.js
@@ -1,4 +1,8 @@
 require('ember-touch');
 require('ember-data');
 require('th-client-core');
+
+require('th-client-core/fixtures/initial');
+
 require('th-client-views');
+
